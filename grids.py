dedf4bdcbb73bdfd7a6fd9de4f5b57d4746f59eb
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2018-12-31 03:57:56 +0100

    Update

--- grids.py
+++ grids.py
@@ -34,6 +34,8 @@ if sys.version_info >= (3,):
 libfapi = misc.load_library('libfapi')
 libcapi = misc.load_library('libcapi')
 
+EPS = 1e-7
+
 # ~= (L+1)**2/3
 LEBEDEV_ORDER = {
       0:    1,
@@ -707,6 +709,117 @@ class Grids(object):
 
     kernel = build
 
+def legendre(n):
+
+    x = numpy.zeros(n)
+    w = numpy.zeros(n)
+
+    e1 = n*(n+1)
+    m = ((n+1)//2)
+    for i in range(1,m+1):
+        mp1mi = m+1-i
+        t = float(4*i-1)*numpy.pi/float(4*n+2)
+        x0 = numpy.cos(t)*(1.0-(1.0-1.0/float(n))/float(8*n*n))
+        pkm1 = 1.0
+        pk = x0
+        for k in range(2,n+1):
+            pkp1 = 2.0*x0*pk-pkm1-(x0*pk-pkm1)/float(k)
+            pkm1 = pk
+            pk = pkp1
+        d1 = float(n)*(pkm1-x0*pk)
+        dpn = d1/(1.0-x0*x0)
+        d2pn = (2.0*x0*dpn-e1*pk)/(1.0-x0*x0)
+        d3pn = (4.0*x0*d2pn+(2.0-e1)*dpn)/(1.0-x0*x0)
+        d4pn = (6.0*x0*d3pn+(6.0-e1)*d2pn)/(1.0-x0*x0)
+        u = pk/dpn
+        v = d2pn/dpn
+        h = -u*(1.0+0.5*u*(v+u*(v*v-d3pn/(3.0*dpn))))
+        p = pk+h*(dpn+0.5*h*(d2pn+h/3.0*(d3pn+0.25*h*d4pn)))
+        dp = dpn+h*(d2pn+0.5*h*(d3pn+h*d4pn/3.0))
+        h = h-p/dp
+        xtemp = x0+h
+        x[mp1mi-1] = xtemp
+        fx = d1-h*e1*(pk+0.5*h*(dpn+h/3.0*(d2pn+0.25*h*(d3pn+0.2*h*d4pn))))
+        w[mp1mi-1] = 2.0*(1.0-xtemp*xtemp)/fx/fx
+
+    if ((n%2) == 1):
+        x[0] = 0.0
+    nmove = ((n+1)//2)
+    ncopy = n-nmove
+    for i in range(1,nmove+1):
+        iback = n+1-i
+        x[iback-1] = x[iback-ncopy-1]
+        w[iback-1] = w[iback-ncopy-1]
+    for i in range(1,n-nmove+1):
+        x[i-1] = -x[n-i]
+        w[i-1] = w[n-i]
+
+    return x, w
+
+def rquad(nr,r0,rfar,rad,iqudr,mapr):
+
+    rmesh = numpy.zeros(nr)
+    dvol = numpy.zeros(nr)
+    dvoln = numpy.zeros(nr)
+ 
+    if (rfar-r0 <= 0.001):
+        raise RuntimeError('rmax < rmin ??') 
+
+    # Determine eta parameter in case of radial mapping
+    rfarc = rfar - r0
+    if (mapr == 1):
+        eta = 2.0*rad/rfarc
+    elif (mapr == 2):
+        eta = 2.0*numpy.exp(-rfarc/rad)/(1.0-numpy.exp(-rfarc/rad))
+    elif (mapr == 0):
+        eta = 0.0
+    else:    
+        raise NotImplementedError('Only becke or exp mapping available') 
+
+    if (iqudr == 1):
+        xr, rwei = legendre(nr)
+    else:    
+        raise NotImplementedError('Only legendre quadrature available') 
+
+    # Determine abscissas and volume elements.
+    # for finite range (a..b) the transformation is y = (b-a)*x/2+(b+a)/2
+    # x = (b-a)*0.5*x+(b+a)*0.5
+    # w = w*(b-a)*0.5
+    if (mapr == 0):
+        for i in range(nr):
+            aa = (rfar-r0)/2.0
+            bb = (rfar+r0)/2.0
+            u = xr[i]
+            r = aa*u+bb
+            rmesh[i] = r
+            dvoln[i] = r*aa
+            dvol[i] = dvoln[i]*r
+    elif (mapr == 1):
+        for i in range(nr):
+            u = xr[i]
+            den = (1.0-u+eta)
+            r = rad*(1.0+u)/den + r0
+            rmesh[i] = r
+            if (numpy.abs(den) >= EPS):
+                dvoln[i] = rad*(2.0+eta)/den/den*r
+            else:
+                dvoln[i] = 0.0
+            dvol[i] = dvoln[i]*r
+    elif (mapr == 2):
+        for i in range(nr):
+            u = xr[i]
+            den = (1.0-u+eta)
+            r = rad*numpy.log((2.0+eta)/den) + r0
+            rmesh[i] = r
+            if (numpy.abs(den) >= EPS):
+                dvoln[i] = r*rad/den
+            else:
+                dvoln[i] = 0.0
+            dvol[i] = dvoln[i]*r
+
+    return rmesh, rwei, dvol, dvoln
+
+
 if __name__ == '__main__':
 
     npang = 5810
@@ -722,9 +835,22 @@ if __name__ == '__main__':
     g.verbose = 4
     g.prune = None
     g.build()
-    print(g.coords.shape)
     with open('bgrid.txt', 'w') as f2:
+        f2.write('# Dimension of grid %d\n' % g.coords.shape[0])
         f2.write('# Point weight\n')
         for i in range(g.weights.shape[0]):
             f2.write('%d   %.6f  %.6f  %.6f  %.6f\n' % \
             ((i+1), g.coords[i,0], g.coords[i,1], g.coords[i,2], g.weights[i]))
+
+    nr = 10
+    x, w = legendre(nr) 
+    print('Legendre points %s' % x)
+    print('Legendre weigths %s' % w)
+
+    r0 = 0
+    rfar = 2
+    rad = 1.5
+    iqudr = 1
+    mapr = 1
+    rm, rw, dv, dvn = rquad(nr,r0,rfar,rad,iqudr,mapr)
+
--- h2o.py
+++ h2o.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import numpy, time
-from pyscf import gto, scf, lib, dft
+from pyscf import gto, scf, lib, dft, ao2mo
 from pyscf.tools import wfn_format
 
 name = 'h2o'
@@ -30,5 +30,52 @@ mf.kernel()
 wfn_file = name + '.wfn'
 with open(wfn_file, 'w') as f2:
     wfn_format.write_mo(f2, mol, mf.mo_coeff[:,mf.mo_occ>0], \
-    mo_occ=mf.mo_occ[mf.mo_occ>0])
+    mo_occ=mf.mo_occ[mf.mo_occ>0], mo_energy=mf.mo_energy[mf.mo_occ>0])
+
+dm = mf.make_rdm1()
+nao = mol.nao_nr()
+
+coords = mf.grids.coords
+weights = mf.grids.weights
+ngrids = len(weights)
+ao = dft.numint.eval_ao(mol, coords, deriv=1)
+rho = dft.numint.eval_rho(mol, ao, dm, xctype='GGA')
+lib.logger.info(mf,'Rho = %.12f' % numpy.einsum('i,i->', rho[0], weights))
+ex, vx = dft.libxc.eval_xc('rPW86,', rho)[:2]
+ec, vc = dft.libxc.eval_xc(',PBE', rho)[:2]
+ex = numpy.einsum('i,i,i->', ex, rho[0], weights)
+ec = numpy.einsum('i,i,i->', ec, rho[0], weights)
+
+s = mol.intor('int1e_ovlp')
+t = mol.intor('int1e_kin')
+v = mol.intor('int1e_nuc')
+eri_ao = ao2mo.restore(1,mf._eri,nao)
+eri_ao = eri_ao.reshape(nao,nao,nao,nao)
+
+enuc = mol.energy_nuc() 
+ekin = numpy.einsum('ij,ji->',t,dm)
+pop = numpy.einsum('ij,ji->',s,dm)
+elnuce = numpy.einsum('ij,ji->',v,dm)
+lib.logger.info(mf,'Population : %12.6f' % pop)
+lib.logger.info(mf,'Kinetic energy : %12.6f' % ekin)
+lib.logger.info(mf,'Nuclear Atraction energy : %12.6f' % elnuce)
+lib.logger.info(mf,'Nuclear Repulsion energy : %12.6f' % enuc)
+bie1 = numpy.einsum('ijkl,ij,kl->',eri_ao,dm,dm)*0.5 # J
+bie2 = numpy.einsum('ijkl,il,jk->',eri_ao,dm,dm)*0.25 # XC
+pairs1 = numpy.einsum('ij,kl,ij,kl->',dm,dm,s,s) # J
+pairs2 = numpy.einsum('ij,kl,li,kj->',dm,dm,s,s)*0.5 # XC
+pairs = (pairs1 - pairs2)
+lib.logger.info(mf,'Coulomb Pairs : %12.6f' % (pairs1))
+lib.logger.info(mf,'XC Pairs : %12.6f' % (pairs2))
+lib.logger.info(mf,'Pairs : %12.6f' % pairs)
+lib.logger.info(mf,'J energy : %12.6f' % bie1)
+lib.logger.info(mf,'XC energy : %12.6f' % -bie2)
+lib.logger.info(mf,'EE energy : %12.6f' % (bie1-bie2))
+etot = enuc + ekin + elnuce + bie1 - bie2
+lib.logger.info(mf,'HF Total energy : %12.6f' % etot)
+lib.logger.info(mf,'Ex : %12.6f' % ex)
+lib.logger.info(mf,'Ec : %12.6f' % ec)
+lib.logger.info(mf,'Exc : %12.6f' % (ex+ec))
+etot = enuc + ekin + elnuce + bie1 + ex + ec
+lib.logger.info(mf,'DFT Total energy : %12.6f' % etot)
 
