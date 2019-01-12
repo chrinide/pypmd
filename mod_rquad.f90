13b1feafde10418406942ba3d5b3cfe71ae761c3
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2019-01-13 00:53:40 +0100

    Update

--- mod_rquad.f90
+++ mod_rquad.f90
@@ -1,7 +1,131 @@
+module mod_rquad
+
+  use mod_prec, only: rp, ip
+  implicit none
+  private
+
+  public :: rquad
+
+contains
+
+! Setup radial quadratures
+subroutine rquad (r0,rfar,rad,nr,rmesh,dvol,dvoln,rwei,mapr,iqudr)
+
+  use mod_io, only: faterr, ferror
+  implicit none
+  real(kind=rp), parameter :: epsden = 1d-7
+ 
+  integer(kind=ip), intent(in) :: nr, mapr, iqudr
+  real(kind=rp), intent(in) :: r0, rfar, rad 
+  real(kind=rp), intent(out) :: rmesh(nr), dvol(nr), dvoln(nr), rwei(nr)
+ 
+  integer(kind=ip) :: n
+  real(kind=rp) :: u, aa, bb, r, rfarc, den, eta
+  real(kind=rp), dimension(nr) :: xr
+ 
+  ! Make checks and init
+  if (rfar-r0.le.0.001_rp) then
+    call ferror ('rquad', 'rmax < rmin ???', faterr)
+  end if
+  if (iqudr.lt.1 .or. iqudr.gt.9) then
+    call ferror ('rquad', 'not allowed radial quadrature', faterr)
+  end if
+  if (mapr.lt.0 .or. mapr.gt.3) then
+    call ferror ('rquad', 'not allowed radial mapping', faterr)
+  end if
+  rmesh = 0.0_rp
+  dvoln = 0.0_rp
+  dvol = 0.0_rp
+  rwei = 0.0_rp
+  rfarc = 0.0_rp
+  eta = 0.0_rp
+
+  ! Determine eta parameter in case of radial mapping
+  rfarc = rfar - r0
+  if (mapr.eq.1) then
+    eta = 2.0_rp*rad/rfarc
+  else if (mapr.eq.2) then
+    eta = 2.0_rp*exp(-rfarc/rad)/(1.0_rp-exp(-rfarc/rad))
+  end if
+
+  ! Determine abscissas and weights of the quadrature.
+  if (iqudr.eq.1) then 
+    call legendre_ss (xr,rwei,nr)
+  else if (iqudr.eq.2) then 
+    call chebyshev1 (xr,rwei,nr)
+  else if (iqudr.eq.3) then 
+    call chebyshev2 (xr,rwei,nr)
+  else if (iqudr.eq.4) then  
+    call fejer1 (xr,rwei,nr)
+  else if (iqudr.eq.5) then  
+    call fejer2 (xr,rwei,nr)
+  else if (iqudr.eq.6) then  
+    call lobatto (xr,rwei,nr)
+  else if (iqudr.eq.7) then  
+    call radau (xr,rwei,nr)
+  else if (iqudr.eq.8) then  
+    call pjt (xr,rwei,nr)
+  else if (iqudr.eq.9) then 
+    call clenshaw_curtis (xr,rwei,nr)
+  else
+    call ferror ('rquad', 'not allowed radial quadrature', faterr)
+  end if
+
+  ! Determine abscissas and volume elements.
+  if (mapr.eq.0) then
+    ! for finite range (a..b) the transformation is y = (b-a)*x/2+(b+a)/2
+    ! x = (b-a)*0.5_rp*x+(b+a)*0.5_rp
+    ! w = w*(b-a)*0.5_rp
+    do n = 1,nr
+      aa = (rfar-r0)/2.0_rp
+      bb = (rfar+r0)/2.0_rp
+      u = xr(n)
+      r = aa*u+bb
+      rmesh(n) = r
+      dvoln(n) = r*aa
+      dvol(n) = dvoln(n)*r
+    end do  
+  else if (mapr.eq.1) then
+    do n = 1,nr
+      u = xr(n)
+      den = (1-u+eta)
+      r = rad*(1+u)/den + r0
+      rmesh(n) = r
+      if (abs(den).gt.epsden) then !this
+        dvoln(n) = rad*(2.0_rp+eta)/den/den*r
+      else
+        dvoln(n) = 0.0_rp
+      endif
+      dvol(n) = dvoln(n)*r
+    enddo
+  else if (mapr.eq.2) then
+    do n = 1,nr
+      u = xr(n)
+      den = (1.0_rp-u+eta)
+      r = rad*log((2.0_rp+eta)/den) + r0
+      rmesh(n) = r
+      if (abs(den).gt.epsden) then !this
+        dvoln(n) = r*rad/den
+      else
+        dvoln(n) = 0.0_rp
+      endif
+      dvol(n) = dvoln(n)*r
+    end do
+  else if (mapr.eq.3) then
+    do n = 1,nr
+      u = xr(n)
+      r = (1.0_rp+u)*rfarc/2.0_rp + r0
+      rmesh(n) = r
+      dvoln(n) = r*rfarc/2.0_rp
+      dvol(n) = dvoln(n)*r
+    end do
+  end if
+ 
+  end subroutine
+
 ! A tanh-sinh quadrature rule.
 subroutine tanh_sinh (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -35,7 +159,6 @@ end subroutine
 
 subroutine chebyshev1 (x,w,n)
   
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
   real(kind=rp), parameter :: half = 0.5_rp
@@ -54,7 +177,6 @@ end subroutine
 
 subroutine chebyshev2 (x,w,n)
   
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -75,7 +197,6 @@ end subroutine
 ! quadrature of the first kind using the Perez-Jorda transformation.
 subroutine pjt (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
   real(kind=rp), parameter :: twothird = 2.0_rp/3.0_rp
@@ -107,7 +228,6 @@ end subroutine
 ! the old ones. One such sequence would be N(K) = 2*K+1 for K = 0, 1, 2, ...
 subroutine clenshaw_curtis (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -152,7 +272,6 @@ end subroutine
 
 subroutine fejer1 (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -188,7 +307,6 @@ end subroutine
 
 subroutine fejer2 (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -237,7 +355,6 @@ end subroutine
 ! are always abscissas.
 subroutine lobatto (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -283,7 +400,6 @@ end subroutine
 ! polynomials up to X**(2*N-2).
 subroutine radau (x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -336,7 +452,6 @@ end subroutine
 !   integral (-1 <= x <= 1) f(x) dx
 subroutine legendre_ss (x,w,n)
 
-  use mod_prec, only: rp, ip
   implicit none
 
   integer(kind=ip) :: n, i
@@ -381,7 +496,6 @@ end subroutine
 ! High precision is a good idea for this routine
 subroutine gauleg (x1,x2,x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -443,7 +557,6 @@ end subroutine
 ! This routine computes the points and weights of the Gen-Clurtis quadrature
 subroutine genclcu (a,b,x,w,n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
   real(kind=rp), parameter :: half = 0.5_rp
@@ -498,7 +611,6 @@ end subroutine
 ! Computes a tanh quadrature rule.
 subroutine tanh_rule (x,w,n,h)
  
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -523,7 +635,6 @@ end subroutine
 ! Computes a tanh-sinh quadrature rule.
 subroutine tanh_sinh_rule (x,w,n,h)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -551,7 +662,6 @@ end subroutine
 
 subroutine legendre_ss_recur (p2, dp2, p1, x, n, c)
 
-  use mod_prec, only: rp, ip
   implicit none
 
   integer(kind=ip) :: n, i
@@ -575,7 +685,6 @@ end subroutine
 
 subroutine legendre_ss_root (x, n, dp2, p1, c)
 
-  use mod_prec, only: rp, ip
   implicit none
   integer(kind=ip), parameter :: step_max = 30
 
@@ -598,7 +707,6 @@ end subroutine
 ! Computes N as a function of H and TOL.
 subroutine tanh_h_to_n (h, tol, n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -621,7 +729,6 @@ end subroutine
 ! Computes H as a function of M.
 subroutine tanh_m_to_h (m, h)
 
-  use mod_prec, only: rp, ip
   implicit none
 
   real(kind=rp) :: h
@@ -640,7 +747,6 @@ end subroutine
 ! Computes N as a function of H and TOL for tanh-sinh rule.
 subroutine tanh_sinh_h_to_n (h, tol, n)
 
-  use mod_prec, only: rp, ip
   use mod_param, only: pi
   implicit none
 
@@ -661,3 +767,5 @@ subroutine tanh_sinh_h_to_n (h, tol, n)
   end do
 
 end subroutine
+
+end module mod_rquad
