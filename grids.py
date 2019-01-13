14783cccf4b9419e02acb0dd303ea06297a1f4c0
Jose Luis Casals Sainz <jluiscasalssainz@gmail.com>
2019-01-13 01:37:55 +0100

    Update

--- grids.py
+++ grids.py
@@ -16,6 +16,7 @@ if sys.version_info >= (3,):
 
 libfapi = misc.load_library('libfapi')
 
+NELEC_ERROR_TOL = 0.02
 EPS = 1e-7
 HMINIMAL = numpy.finfo(numpy.float64).eps
 LEBEDEV_NGRID = numpy.asarray((
@@ -259,6 +260,18 @@ def rquad(nr,r0,rfar,rad,iqudr,mapr):
                 dvoln[i] = 0.0
             dvol[i] = dvoln[i]*r
     return rmesh, rwei, dvol, dvoln
+
+#def prune_small_rho_grids(self, grids):
+#    rho = evaluator.eval_rho(self, grids.coords)
+#    n = numpy.dot(rho, grids.weights)
+#    if abs(n-self.nelectron) < NELEC_ERROR_TOL*n:
+#        rho *= grids.weights
+#        idx = abs(rho) > self.small_rho_cutoff/grids.weights.size
+#        logger.debug(self,'Drop grids %d',
+#                     grids.weights.size - numpy.count_nonzero(idx))
+#        grids.coords = numpy.asarray(grids.coords[idx], order='C')
+#        grids.weights = numpy.asarray(grids.weights[idx], order='C')
+#    return grids
 
 if __name__ == '__main__':
     nr = 10
--- misc.py
+++ misc.py
@@ -218,3 +218,58 @@ class H5TmpFile(h5py.File):
             self.close()
         except ValueError:  # if close() is called twice
             pass
+
+def cartesian_prod(arrays, out=None):
+    '''
+    Generate a cartesian product of input arrays.
+    http://stackoverflow.com/questions/1208118/using-numpy-to-build-an-array-of-all-combinations-of-two-arrays
+
+    Args:
+        arrays : list of array-like
+            1-D arrays to form the cartesian product of.
+        out : ndarray
+            Array to place the cartesian product in.
+
+    Returns:
+        out : ndarray
+            2-D array of shape (M, len(arrays)) containing cartesian products
+            formed of input arrays.
+
+    Examples:
+
+    >>> cartesian_prod(([1, 2, 3], [4, 5], [6, 7]))
+    array([[1, 4, 6],
+           [1, 4, 7],
+           [1, 5, 6],
+           [1, 5, 7],
+           [2, 4, 6],
+           [2, 4, 7],
+           [2, 5, 6],
+           [2, 5, 7],
+           [3, 4, 6],
+           [3, 4, 7],
+           [3, 5, 6],
+           [3, 5, 7]])
+
+    '''
+    arrays = [numpy.asarray(x) for x in arrays]
+    dtype = numpy.result_type(*arrays)
+    nd = len(arrays)
+    dims = [nd] + [len(x) for x in arrays]
+
+    if out is None:
+        out = numpy.empty(dims, dtype)
+    else:
+        out = numpy.ndarray(dims, dtype, buffer=out)
+    tout = out.reshape(dims)
+
+    shape = [-1] + [1] * nd
+    for i, arr in enumerate(arrays):
+        tout[i] = arr.reshape(shape[:nd-i])
+
+    return tout.reshape(nd,-1).T
+
+def prange(start, end, step):
+    for i in range(start, end, step):
+        yield i, min(i+step, end)
+
